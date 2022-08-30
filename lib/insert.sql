d4ee7cd16be1ab57e6cde06f3d4ec2a74a3e0dec
Miriam Anyango <minaoanya@gmail.com>
2022-08-30 11:26:17 +0300

    commit tree 8640cf49486f9ddac1597c7cc765f04636bf2af7

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', 'Black', 'Choleric',True );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 50, 'M', 'Brown', 'Melancholic', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 8, 'F', 'Grey', 'Melancholic', False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 2, 'M', 'White', 'Sanguine', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 7, 'F', 'Black', 'Choleric', False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 10, 'M', 'Black', 'Phlegmatic', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 6, 'F', 'White', 'Sanguine', True);
+INSERT INTO bears (name) VALUES (NULL);
